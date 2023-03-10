fd3f784bd6fa9d8831e2efe5ea64a691ce725f92
gw1106 <gw1106@naver.com>
2023-03-10 10:53:41 +0900

    Master Commit2 그레이 스케일 효과 추가

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import cv2
 
+#영상 소스 열기
 src1 = cv2.imread('c:/images/picture99.jpg')
 
+#영상처리 알고리즘
+dst1 = cv2.cvtColor(src1, cv2.COLOR_RGB2GRAY)
 
 
+
+
+
+#영상 디스플레이
 cv2.imshow('src1', src1)
+cv2.imshow('dst1', dst1)
 
 
-
-
-cv2.waitkey(0)
-cv2.destroyAllWindows()
+#영상종료 및 마무리
+cv2.waitkey(0)             #아무키 누르면 종료
+cv2.destroyAllWindows()    #메모리 해제
